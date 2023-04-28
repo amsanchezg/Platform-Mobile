e69c6cb84344d234b4b79f8eea69a8f7d2b9bd41
Antonio Miguel Sánchez Gómez <amsanchezg@esi-murcia.es>
2023-04-28 13:52:02 +0200

    Bakeo de niveles y ajuste de camara

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public LayerMask LayerWall;
    public GameObject target;
      public Vector3 offset = new Vector3(-3,2,3);
      public float damping = 1.0f;
@@ -14,8 +15,23 @@ public class CameraFollow : MonoBehaviour
      
      // Update is called once per frame
      void Update () {
-        //Mediante el offset y el target, se establece la posicion deseada
-         Vector3 desiredPosition = target.transform.position + offset;
+        //Vector3 dir = transform.position - target.transform.position;
+        RaycastHit hit;
+        Vector3 desiredPosition;
+        if (Physics.Raycast(target.transform.position, offset, out hit, Vector3.Distance(target.transform.position, transform.position), LayerWall))
+        {
+
+            desiredPosition = hit.point;
+        }
+        else
+        {
+
+            //Mediante el offset y el target, se establece la posicion deseada
+            desiredPosition = target.transform.position + offset;
+
+        }
+
+
          Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
          transform.position = position;
      }
